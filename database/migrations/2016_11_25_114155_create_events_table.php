94c48339a7bb9ef1e8159e30a63abc5fbb71e08a
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-25 17:19:28 +0530

    Add new migrations for departments,scores and events table (NOT FINAL)

---
+++ database/migrations/2016_11_25_114155_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('event_id');
+            $table->string("name");
+            $table->string("day"); //can be (day) 1 2 or 3
+            $table->string("score_1");
+            $table->string("score_2");
+            $table->string("score_3");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
