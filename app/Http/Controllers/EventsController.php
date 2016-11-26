e8b265e9fead00caa5f168ba024feb9bfd51b039
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-26 15:02:13 +0530

    Add api method in EventsController for GET: /api/events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,7 +2,25 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class EventsController extends Controller{
+    public function CheckDay($day){
+      return $day <= 3;
+    }
     public function GetEvents(Request $request){
-      return $request;
+      $data = [];
+      $data['type'] = 'events';
+      $data['day'] = $request['day'];
+      $day = $request['day'];
+      if($this->CheckDay($day)){
+        $data['status'] = '200 OK';
+        $data['message'] = 'day found';
+        $events = app('db')->select('select name from events where day = '.$day.'');
+        $data['data'] = $events;
+      }
+      else{
+        $data['status'] = '404 NOT FOUND';
+        $data['message'] = 'requested day not found';
+        $data['data'] = NULL;
+      }
+      return json_encode($data);
     }
 }
